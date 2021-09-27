20b55dc127ca909f8f468a1973f95b59e83b31e7
DAbraham2 <96dane@gmail.com>
2021-09-27 11:52:33 +0200

    second done.

--- src/main/java/hu/bme/aut/retelab2/controller/AdController.java
+++ src/main/java/hu/bme/aut/retelab2/controller/AdController.java
@@ -4,10 +4,9 @@ import hu.bme.aut.retelab2.domain.Ad;
 import hu.bme.aut.retelab2.repository.AdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/ads")
@@ -15,6 +14,11 @@ public class AdController {
 
     @Autowired
     private AdRepository adRepository;
+
+    @GetMapping
+    public List<Ad> getAll(@RequestParam(defaultValue = "0") int min, @RequestParam(defaultValue = "10000000") int max) {
+        return adRepository.get(min, max);
+    }
 
     @PostMapping
     public Ad create(@RequestBody Ad data) {
--- src/main/java/hu/bme/aut/retelab2/repository/AdRepository.java
+++ src/main/java/hu/bme/aut/retelab2/repository/AdRepository.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public class AdRepository {
@@ -17,5 +18,14 @@ public class AdRepository {
     public Ad save(Ad data){
         data.setCreationDate(new Date(System.currentTimeMillis()));
         return em.merge(data);
+    }
+
+    public List<Ad> get(int min, int max) {
+        List<Ad> res = em.createQuery("SELECT n FROM Ad n WHERE n.price BETWEEN ?1 AND ?2", Ad.class)
+                .setParameter(1, min)
+                .setParameter(2,max)
+                .getResultList();
+
+        return res;
     }
 }
